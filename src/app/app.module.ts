12c93ca6958c0e40f02809522c6217be02594c11
Axel Buteau <buteauaxel@gmail.com>
2018-04-17 15:54:44 +0200

    avis component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { DemoComponent } from './demo/demo.component';
+import { BandeauComponentComponent } from './bandeau-component/bandeau-component.component';
+import { AvisComponentComponent } from './avis-component/avis-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    DemoComponent,
+    BandeauComponentComponent,
+    AvisComponentComponent
+     ],
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot()
