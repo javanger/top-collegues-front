e6dfb1623c42d712b63bc47e181ea390962d7921
Guillaume <guillaume.gobert@gmail.com>
2018-04-17 17:31:01 +0200

    mise a jour

--- src/app/demo/liste-collegue/liste-collegue.component.ts
+++ src/app/demo/liste-collegue/liste-collegue.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Collegue } from '../../models';
 
 @Component({
   selector: 'app-liste-collegue',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListeCollegueComponent implements OnInit {
 
+  list: Array<Collegue> = [new Collegue("https://img.stackshare.io/stack/332/7d9825fd0e38f5e05831d226afc41d9c.jpeg","travis1",100), 
+                           new Collegue("https://img.stackshare.io/stack/332/7d9825fd0e38f5e05831d226afc41d9c.jpeg","travis2",-500), 
+                           new Collegue("https://img.stackshare.io/stack/332/7d9825fd0e38f5e05831d226afc41d9c.jpeg","travis3",250), 
+                           new Collegue("https://img.stackshare.io/stack/332/7d9825fd0e38f5e05831d226afc41d9c.jpeg","travis4",10), 
+                           new Collegue("https://img.stackshare.io/stack/332/7d9825fd0e38f5e05831d226afc41d9c.jpeg","travis5",-1000), 
+                           new Collegue("https://img.stackshare.io/stack/332/7d9825fd0e38f5e05831d226afc41d9c.jpeg","travis6",1000)];
   constructor() { }
 
   ngOnInit() {
