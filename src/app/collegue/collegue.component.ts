542c2d1c214b61e88bcb08c83c0415537cab8944
Axel Buteau <buteauaxel@gmail.com>
2018-04-19 16:15:27 +0200

    correction bouton

--- src/app/collegue/collegue.component.ts
+++ src/app/collegue/collegue.component.ts
@@ -2,31 +2,20 @@ import { Component, OnInit, Input } from "@angular/core";
 import { Avis, Collegue } from "../models";
 import { CollegueService } from "../services/collegue.service";
 
-
 @Component({
   selector: "app-collegue",
   templateUrl: "./collegue.component.html",
   styleUrls: ["./collegue.component.scss"]
 })
 export class CollegueComponent implements OnInit {
-   
-  @Input() collegue:Collegue ;
-  
-  constructor(private cService:CollegueService) {   }
+  @Input() collegue: Collegue;
 
-  ngOnInit() {
-  
+  constructor(private cService: CollegueService) {}
+
+  ngOnInit() {}
+  ajoutAvis(event) {
+    this.cService
+      .donnerUnAvis(this.collegue, event)
+      .then(c => (this.collegue = c));
   }
-    ajoutAvis(event) {
-    if (event == Avis.AIMER) {
-      this.collegue.note += 10;
-      this.cService.donnerUnAvis(this.collegue, event)
-    } else if (event == Avis.DETESTER) {
-      this.collegue.note -= 10;
-      this.cService.donnerUnAvis(this.collegue, event)
-    }
-  }
-  
-
-
 }
