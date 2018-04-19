2e4b4976c53ba08fc7a1d9d5f1dbcf662987becb
Guillaume <guillaume.gobert@gmail.com>
2018-04-19 12:06:05 +0200

    ajout back heroku

--- src/app/services/collegue.service.ts
+++ src/app/services/collegue.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import {HttpClient,HttpHeaders} from '@angular/common/http';
 import { Avis, Collegue } from '../models';
+import {environment} from '../../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({
     "Content-Type": "application/json"
   })
 };
+const URL_BACKEND = environment.backendUrl;
 
 @Injectable()
 export class CollegueService {
@@ -14,7 +16,7 @@ export class CollegueService {
   constructor(private _http:HttpClient) { }
 
   listerCollegues():Promise<Collegue[]>  {
-    return this._http.get("http://localhost:8080/collegues")
+    return this._http.get(URL_BACKEND+"/collegues")
     .toPromise()
     .then((data: any) => {  
       return data.map((d:any) => new Collegue(d.pseudo, d.score,d.photo))        
@@ -22,7 +24,7 @@ export class CollegueService {
   }
 
   donnerUnAvis(unCollegue:Collegue, avis:Avis):Promise<Collegue>  {
-    return this._http.patch('http://localhost:8080/collegues/' + unCollegue.pseudo,
+    return this._http.patch(URL_BACKEND +"/collegues/" + unCollegue.pseudo,
                             {action : avis},
                             httpOptions)
     .toPromise()
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: true,
+
+  backendUrl: 'https://guilome-top-collegues-backend.herokuapp.com'
 };
